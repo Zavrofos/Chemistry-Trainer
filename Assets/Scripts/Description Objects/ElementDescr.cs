233de92c01136eb17e7c432aad785d05e58762d1
Zavrofos <rebrin.na98@gmail.com>
2023-11-15 13:50:12 +0600

    Added start scripts and ScriptableObject ElementDescr

---
+++ Assets/Scripts/Description Objects/ElementDescr.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Description_Objects
+{
+    [CreateAssetMenu(fileName = "Element", menuName = "Create Description/Element")]
+    public class ElementDescr : ScriptableObject
+    {
+        public string Name;
+        public string Formula;
+        public EnvironmentType EnvironmentType;
+    }
+}
