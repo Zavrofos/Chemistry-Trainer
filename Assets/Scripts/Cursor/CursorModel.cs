bded00ddc8f2250d2ddb54bf99b6c988a473621a
Zavrofos <rebrin.na98@gmail.com>
2023-11-15 18:14:54 +0600

    Added event

--- Assets/Scripts/Cursor/CursorModel.cs
+++ Assets/Scripts/Cursor/CursorModel.cs
@@ -1,7 +1,17 @@
-﻿namespace Cursor
+﻿using System;
+using UnityEngine;
+
+namespace Cursor
 {
     public class CursorModel
     {
+        public GameObject TargetAtGunPoint { get; private set; }
+        public event Action ChangedTarget;
         
+        public void ChangeTarget(GameObject newTarget)
+        {
+            TargetAtGunPoint = newTarget;
+            ChangedTarget?.Invoke();
+        }
     }
 }
--- Assets/Scripts/Cursor/HoverUpdater.cs
+++ Assets/Scripts/Cursor/HoverUpdater.cs
@@ -1,7 +1,19 @@
-﻿namespace Cursor
+﻿using GameModelDir;
+using GameViewDir;
+
+namespace Cursor
 {
     public class HoverUpdater : IUpdater
     {
+        private readonly GameModel _gameModel;
+        private readonly GameView _gameView;
+
+        public HoverUpdater(GameModel gameModel, GameView gameView)
+        {
+            _gameModel = gameModel;
+            _gameView = gameView;
+        }
+        
         public void Update()
         {
             
