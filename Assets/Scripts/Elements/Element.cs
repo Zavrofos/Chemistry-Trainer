2509e918c8a31b707abbf8f5ce336f2b95ea8dd3
Zavrofos <rebrin.na98@gmail.com>
2023-11-15 15:08:03 +0600

    Added class Element

---
+++ Assets/Scripts/Elements/Element.cs
@@ -0,0 +1,13 @@
+﻿public class Element
+{
+    public string Name;
+    public string Formula;
+    public EnvironmentType EnvironmentType;
+
+    public Element(string name, string formula, EnvironmentType environmentType)
+    {
+        Name = name;
+        Formula = formula;
+        EnvironmentType = environmentType;
+    }
+}
