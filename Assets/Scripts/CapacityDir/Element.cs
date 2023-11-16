0e8bb52b7064e8b7e7b4d88e9788cda2cd41b61c
Zavrofos <rebrin.na98@gmail.com>
2023-11-16 18:17:38 +0600

    Added class Element

---
+++ Assets/Scripts/CapacityDir/Element.cs
@@ -0,0 +1,21 @@
+﻿using Description_Scripts;
+using UnityEngine;
+
+namespace CapacityDir
+{
+    public class NewElement
+    {
+        public readonly string Name;
+        public readonly string Formula;
+        public readonly EnvironmentType EnvironmentType;
+        public readonly Color Color;
+
+        public NewElement(string name, string formula, EnvironmentType environmentType, Color color)
+        {
+            Name = name;
+            Formula = formula;
+            EnvironmentType = environmentType;
+            Color = color;
+        }
+    }
+}
