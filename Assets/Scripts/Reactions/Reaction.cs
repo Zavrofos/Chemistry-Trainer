10d677fe36e2a671200b9c16ddf5425ef3f8bd07
Zavrofos <rebrin.na98@gmail.com>
2023-11-16 20:30:42 +0600

    Added event

--- Assets/Scripts/Reactions/Reaction.cs
+++ Assets/Scripts/Reactions/Reaction.cs
@@ -1,15 +1,23 @@
-﻿using Effects;
+﻿using System;
+using CapacityDir;
+using Effects;
 
 public class Reaction
 {
     public string[] Elements;
     public string[] ResultElements;
     public IEffect[] Effects;
+    public event Action<CapacityView> StartedReaction; 
 
     public Reaction(string[] elements, string[] resultElements, IEffect[] effects)
     {
         Elements = elements;
         ResultElements = resultElements;
         Effects = effects;
+    }
+
+    public void StartReaction(CapacityView capacity)
+    {
+        StartedReaction?.Invoke(capacity);
     }
 }
