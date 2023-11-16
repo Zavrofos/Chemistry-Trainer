290babf100dba2fb855ee34c3f7f8b4949113a82
Zavrofos <rebrin.na98@gmail.com>
2023-11-16 20:29:17 +0600

    Added MixElementsPresenter

---
+++ Assets/Scripts/Cursor/MixElementsPresenter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CapacityDir;
+using GameModelDir;
+using GameViewDir;
+
+namespace Cursor
+{
+    public class MixElementsPresenter : IPresenter
+    {
+        private readonly GameModel _gameModel;
+        private readonly GameView _gameView;
+
+        public MixElementsPresenter(GameModel gameModel, GameView gameView)
+        {
+            _gameModel = gameModel;
+            _gameView = gameView;
+        }
+
+        public void Subscribe()
+        {
+            _gameModel.CursorModel.ClickedMouse0 += OnMixElements;
+        }
+
+        public void UnSubscribe()
+        {
+            _gameModel.CursorModel.ClickedMouse0 -= OnMixElements;
+        }
+
+        private void OnMixElements()
+        {
+            if (_gameModel.CursorModel.CurrentState != CursorState.CapacityTilt) return;
+            CapacityView capacityViewTarget = _gameModel.CursorModel.TargetAtGunPoint.GetComponent<CapacityView>();
+            CapacityView capacityViewSelected = _gameModel.CursorModel.SelectedTarget.GetComponent<CapacityView>();
+            
+            _gameModel.CapacityesMap[capacityViewTarget.Id].AddElement(_gameModel.CapacityesMap[capacityViewSelected.Id].CurrentElements);
+            _gameModel.CapacityesMap[capacityViewSelected.Id].EmptyCapacity();
+            _gameModel.CursorModel.CurrentState = CursorState.CapacitySelected;
+        }
+    }
+}
