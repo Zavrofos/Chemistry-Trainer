cc389ac4e9f59d4f5374e5d009a1645472ae9d8b
Zavrofos <rebrin.na98@gmail.com>
2023-11-16 14:22:39 +0600

    Added MoveSelectedCapacityToMouseUpdater

---
+++ Assets/Scripts/Cursor/MoveSelectedCapacityToMouseUpdater.cs
@@ -0,0 +1,24 @@
+﻿using GameModelDir;
+using GameViewDir;
+using UnityEngine;
+
+namespace Cursor
+{
+    public class MoveSelectedCapacityToMouseUpdater : IUpdater
+    {
+        private readonly GameModel _gameModel;
+        private readonly GameView _gameView;
+
+        public MoveSelectedCapacityToMouseUpdater(GameModel gameModel, GameView gameView)
+        {
+            _gameModel = gameModel;
+            _gameView = gameView;
+        }
+
+        public void Update()
+        {
+            if (_gameModel.CursorModel.CurrentState != CursorState.CapacitySelected) return;
+            _gameModel.CursorModel.SelectedTarget.transform.position = _gameModel.CursorModel.CursorPosition;
+        }
+    }
+}
